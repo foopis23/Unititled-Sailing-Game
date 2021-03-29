2c2bce4b7ca09b0fe518ba1fe50c10002d89bb7c
Eric Sexton <foopis23@gmail.com>
2021-03-29 16:23:18 -0400

    Initial Commit

---
+++ Assets/Floater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int floaterCount = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
+    
+    private void FixedUpdate()
+    {
+        var position = transform.position;
+        
+        rigidBody.AddForceAtPosition(Physics.gravity/floaterCount, position, ForceMode.Acceleration);
+        
+        var waveHeight = WaveManager.Instance.GetWaveHeight(position);
+        if (!(position.y < waveHeight)) return;
+
+        var displacementMultiplier = Mathf.Clamp01((waveHeight - position.y) / depthBeforeSubmerged) * displacementAmount;
+        rigidBody.AddForceAtPosition(new Vector3(0f, (Math.Abs(Physics.gravity.y) * displacementMultiplier), 0f) / floaterCount,
+            position, ForceMode.Acceleration);
+        rigidBody.AddForce(-rigidBody.velocity * (displacementMultiplier * waterDrag * Time.fixedDeltaTime), ForceMode.VelocityChange);
+        rigidBody.AddTorque(-rigidBody.angularVelocity * (displacementMultiplier * waterAngularDrag * Time.fixedDeltaTime), ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/WaveManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager Instance;
+    
+    public Vector4 waveA;
+    public Vector4 waveB;
+    public Vector4 waveC;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Instance of Wave Manager Already Exists");
+            Destroy(gameObject);
+        }
+    }
+
+    private float GerstnerWave (Vector4 wave, Vector3 p) {
+        var steepness = wave.z;
+        var wavelength = wave.w;
+        var k = 2 * Mathf.PI / wavelength;
+        var c = Mathf.Sqrt(9.8f / k);
+        var d = new Vector2(wave.x, wave.y).normalized;
+        var f = k * (Vector2.Dot(d, new Vector2(p.x, p.z)) - c * Time.time);
+        var a = steepness / k;
+
+        return a * Mathf.Sin(f);
+    }
+
+    public float GetWaveHeight(Vector3 pos)
+    {
+        return GerstnerWave(waveA, pos) + GerstnerWave(waveB, pos) + GerstnerWave(waveC, pos);
+    }
+}
