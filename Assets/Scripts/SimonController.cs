795ba8a7297d39f8218cdcc84bc603ddcbf06728
Eric Sexton <foopis23@gmail.com>
2021-04-04 21:19:15 -0400

    wip simon says

--- Assets/Scripts/SimonController.cs
+++ Assets/Scripts/SimonController.cs
@@ -9,75 +9,142 @@ public class SimonController : MonoBehaviour
     public string[] pattern;
     public float timeBetweenPatterns;
     public float timeBetweenButtons;
-
     public float timeToExpireAttempt = 60;
-    private float lastAttemptTime;
-    private int attemptIndex;
-    private string[] attempt;
-
+    
+    private float _lastAttemptTime;
+    private int _attemptIndex;
+    private int _level;
     private float _lastPatternTime;
     private float _lastButtonTime;
-    private int patternIndex;
+    private int _patternIndex;
 
-
-    // Start is called before the first frame update
+    private bool _displayCurrentLevel = false;
+    
     private void Start()
     {
         _lastPatternTime = Time.time;
         _lastButtonTime = Time.time;
-        patternIndex = 0;
-        lastAttemptTime = -1;
+        _patternIndex = 0;
+        _lastAttemptTime = -1;
+        _attemptIndex = 0;
+        _level = 1;
+        _displayCurrentLevel = true;
     }
-
-    // Update is called once per frame
+    
     private void Update()
     {
-        if (lastAttemptTime > 0)
+        if (_lastAttemptTime > 0)
         {
-            // don't play pattern if currently attempting
-            if (Time.time - lastAttemptTime < timeToExpireAttempt)
+            // Attempt Expired
+            if (Time.time - _lastAttemptTime >= timeToExpireAttempt)
             {
-                return;
+                Fail();
             }
-
-            lastAttemptTime = -1;
-            return;
         }
 
-        
+        if (!_displayCurrentLevel) return;
         if (!(Time.time - _lastPatternTime > timeBetweenPatterns)) return;
-
         if (!(Time.time - _lastButtonTime > timeBetweenButtons)) return;
+            
         _lastButtonTime = Time.time;
         
-        if (patternIndex >= pattern.Length)
+        if (_patternIndex >= _level || _patternIndex >= pattern.Length)
         {
-            patternIndex = 0;
+            _patternIndex = 0;
             _lastPatternTime = Time.time;
+            
+            if (_lastAttemptTime > 0)
+                _displayCurrentLevel = false;
+            
             return;
         }
         
-        simonAnimator.Play($"Activate{pattern[patternIndex]}");
-        patternIndex++;
+        simonAnimator.Play($"Activate{pattern[_patternIndex]}");
+        _patternIndex++;
+    }
+
+    private void Fail()
+    {
+        //TODO: Play Fail Sound Effect
+        
+        _lastAttemptTime = -1;
+        _level = 1;
+        _attemptIndex = 0;
+
+        // Reset Pattern Timer
+        _lastPatternTime = Time.time;
+        _lastButtonTime = Time.time;
+        _displayCurrentLevel = true;
+    }
+
+    private void CompleteLevel()
+    {
+        //TODO: Play Success Sound
+        _level++;
+        _attemptIndex = 0;
+        
+        // reset pattern timers
+        _displayCurrentLevel = true;
+        _lastPatternTime = Time.time;
+        _lastButtonTime = Time.time;
+    }
+
+    private void CompletePuzzle()
+    {
+        // TODO: Play Complete Puzzle Sound
+        // TODO: Activate Beacon
+        
+        _displayCurrentLevel = false;
+        _attemptIndex = 0;
+        _lastAttemptTime = -1;
+    }
+
+    private void HitButton(string color)
+    {
+        _displayCurrentLevel = false;
+        _lastAttemptTime = Time.time;
+        simonAnimator.Play($"Activate{color}");
+        
+        if (!pattern[_attemptIndex].Equals(color))
+        {
+            Fail();
+        }
+        else
+        {
+            // Success
+            _attemptIndex++;
+        }
+        
+        if (_attemptIndex >= _level)
+        {
+            // Level Complete
+            CompleteLevel();
+
+        }
+        
+        if (_level < pattern.Length) return;
+        
+        //puzzle complete
+        CompletePuzzle();
     }
 
     public void PressedYellow(GameObject player)
     {
-        lastAttemptTime = Time.time;
+        HitButton("Yellow");
     }
 
     public void PressedBlue(GameObject player)
     {
-        lastAttemptTime = Time.time;
+        HitButton("Blue");
     }
 
     public void PressedRed(GameObject player)
     {
-        lastAttemptTime = Time.time;
+        HitButton("Red");
     }
 
     public void PressedGreen(GameObject player)
     {
-        lastAttemptTime = Time.time;
+        HitButton("Green");
     }
 }
